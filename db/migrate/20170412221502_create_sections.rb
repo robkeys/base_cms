5972f083c12d9cd5d5f76efd8a8517b0085be966
Rob Keys <robertckeys@gmail.com>
2017-04-12 15:27:09 -0800

    Change visible to is_visible in sections migration

--- db/migrate/20170412221502_create_sections.rb
+++ db/migrate/20170412221502_create_sections.rb
@@ -4,7 +4,7 @@ class CreateSections < ActiveRecord::Migration[5.0]
       t.integer "page_id"
       t.string "name"
       t.integer "position"
-      t.boolean "visible"
+      t.boolean "is_visible"
       t.string "content_type"
       t.text "content"
 
