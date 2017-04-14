5cff7e9410af420da1143585f77efde44815a065
Rob Keys <robertckeys@gmail.com>
2017-04-14 12:59:24 -0800

    create all css as well as views and controllers for Subjects model

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,79 @@
+class SubjectsController < ApplicationController
+
+  layout 'admin'
+
+  before_action :set_subject_count, :only => [:new, :create, :edit, :update]
+
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new({:name => "Default"})
+  end
+
+  def create
+    # Instantiate the new object using form params
+    @subject = Subject.new(subject_params)
+    #save object
+    if @subject.save
+      # if save succeeds rediriect to the index action
+      flash[:notice] = "Subject created successfully."
+      redirect_to(subjects_path)
+    else
+      # if save fails display again using render so form data remains
+      render('new')
+    end
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Find a new object using the form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # If save succeeds, redirect to the show action
+      flash[:notice] = "Subject updated successfully."
+      redirect_to(subject_path(@subject))
+    else
+      # if save fails display again using render so form data remains
+      render('edit')
+    end
+  end
+
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    # Find a new object using the form parameters
+    @subject = Subject.find(params[:id])
+    # Destroy the object
+    @subject.destroy
+    # Redirect to the index action
+    flash[:notice] = "Subject '#{@subject.name}' successfully."
+    redirect_to(subjects_path)
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :is_visible, :created_at)
+  end
+
+  def set_subject_count
+    @subject_count = Subject.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @subject_count += 1
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  # establish resourceful routes for all CRUD actions
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
